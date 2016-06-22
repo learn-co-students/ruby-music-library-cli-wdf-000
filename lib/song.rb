b6b171373a2845607efa729ecb64c3abcf88e99d
uu <giladbarkan@live.com>
2016-06-22 10:07:19 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -45,6 +45,15 @@ class Song
     artist = Artist.find_or_create_by_name(arr[0])
     genre = Genre.find_or_create_by_name(arr[2])
     new_song = self.new(arr[1],artist,genre)
+    new_song
+  end
+
+  def self.create_from_filename(filename)
+    arr = filename.split(/\s+-\s+|.mp3/)
+
+    artist = Artist.find_or_create_by_name(arr[0])
+    genre = Genre.find_or_create_by_name(arr[2])
+    new_song = self.new(arr[1],artist,genre)
     new_song.save
     new_song
   end

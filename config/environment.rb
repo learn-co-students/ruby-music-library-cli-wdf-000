fed422e55904ead6ba86a85ab4262a1837e4a5f0
Esmery Corniel <esmerycornielle@gmail.com>
2016-08-03 16:27:06 -0400

    Done

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,16 @@ require 'bundler'
 Bundler.require
 
 module Concerns
+  module Findable
+
+    def find_by_name(name)
+      all.find{|obj| obj.name == name}
+    end
+
+    def find_or_create_by_name(name)
+      all.find{|obj| obj.name == name} || self.new(name)
+    end
+  end
 end
 
 require_all 'lib'

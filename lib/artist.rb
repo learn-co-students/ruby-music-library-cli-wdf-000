5b4a3ade4c32135a400f0fd973b0f0f7fbe42912
Keo  Bun <keonilrath@gmail.com>
2016-09-19 07:49:08 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,49 @@
+class Artist
+
+  extend Concerns::Findable
+
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    artist.save
+    artist
+  end
+
+  def add_song(song)
+    if song.artist != self
+      song.artist = self
+    end
+
+    if !songs.include?(song)
+      @songs << song
+    end
+  end
+
+  def songs
+    @songs
+  end
+
+  def genres
+    self.songs.map{|song| song.genre}.uniq
+  end
+end
